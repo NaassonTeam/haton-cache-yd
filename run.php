4340eedd795e77030696d1fef060ac7257bed775
NaassonTeam <66871641+NaassonTeam@users.noreply.github.com>
2021-11-27 15:32:25 +0300

    first commit

--- run.php
+++ run.php
@@ -2,11 +2,17 @@
 
 use Ratchet\Server\IoServer;
 use ServerWS\ServerWS;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
 
 require 'vendor/autoload.php';
 
 $server = IoServer::factory(
-    new ServerWS(),
+    new HttpServer(
+        new WsServer(
+            new ServerWS()
+        )
+    ),
     8080
 );
 
--- src/ServerWS.php
+++ src/ServerWS.php
@@ -7,19 +7,42 @@ use Ratchet\ConnectionInterface;
 
 class ServerWS implements MessageComponentInterface
 {
-    public function onOpen(ConnectionInterface $conn)
-    {
+    protected $clients;
+
+    public function __construct() {
+        $this->clients = new \SplObjectStorage;
     }
 
-    public function onMessage(ConnectionInterface $from, $msg)
-    {
+    public function onOpen(ConnectionInterface $conn) {
+        // Store the new connection to send messages to later
+        $this->clients->attach($conn);
+
+        echo "New connection! ({$conn->resourceId})\n";
     }
 
-    public function onClose(ConnectionInterface $conn)
-    {
+    public function onMessage(ConnectionInterface $from, $msg) {
+        $numRecv = count($this->clients) - 1;
+        echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
+            , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
+
+        foreach ($this->clients as $client) {
+            if ($from !== $client) {
+                // The sender is not the receiver, send to each client connected
+                $client->send($msg);
+            }
+        }
     }
 
-    public function onError(ConnectionInterface $conn, \Exception $e)
-    {
+    public function onClose(ConnectionInterface $conn) {
+        // The connection is closed, remove it, as we can no longer send it messages
+        $this->clients->detach($conn);
+
+        echo "Connection {$conn->resourceId} has disconnected\n";
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+        echo "An error has occurred: {$e->getMessage()}\n";
+
+        $conn->close();
     }
 }
