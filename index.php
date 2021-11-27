f8710024d6e38b83c1689fa311577226c185d49b
NaassonTeam <66871641+NaassonTeam@users.noreply.github.com>
2021-11-27 14:39:16 +0300

    first commit

--- index.php
+++ index.php
@@ -14,14 +14,12 @@ $sharedConfig = [
 $sdk = new Aws\Sdk($sharedConfig);
 $s3Client = $sdk->createS3();
 
+$key = 'page_' . md5('https://haton.ru' . $_SERVER['REQUEST_URI']);
+
 $result = $s3Client->getObject([
     'Bucket' => 'static.haton.ru',
-    'Key' => 'page_018ff7cd3eeecb374fca215df761a725'
+    'Key' => $key
 ]);
 
 echo $result['Body'];
 echo $result['LastModified'];
-
-// $key = 'page_' . md5('https://haton.ru' . $_SERVER['REQUEST_URI']);
-// $html = file_get_contents('page_0a90f7e091d6ba352959853bf9621035');
-// echo $html;
